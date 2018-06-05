95016197ae898634c718949829665ada20141cc1
sanyogitapatil <39790766+sanyogitapatil@users.noreply.github.com>
2018-06-05 12:54:45 +0530

    Update sanyogitaCode.R

--- sanyogitaCode.R
+++ sanyogitaCode.R
@@ -1,14 +1,10 @@
-setwd("D:\\Sanyogita\\Upgrad\\Healthcare\\capstone\\Hospital_Revised_FlatFiles_20161110")
-list.files(pattern = ".csv$")
-list.filename<-list.files(pattern=".csv$")
-list.filenames<-  gsub(".csv","",list.filename)
+folder <- "D:\\Sanyogita\\Upgrad\\Healthcare\\capstone\\Hospital_Revised_FlatFiles_20161110/"      # path to folder that holds multiple .csv files
+file_list <- list.files(path=folder, pattern="*.csv") # create list of all .csv files in folder
+list.filenames<-  gsub(".csv","",file_list)
 list.filenames<-  gsub(" ","-",list.filenames)
 list.filenames<-  gsub("---","-",list.filenames)
-
-# create a loop to read in your data
-for (i in 1:length(list.filename))
-{
-  list.data[[i]]<-read.csv(list.filename[i])
-}
-names(list.data)<-list.filenames
-list.data[1]
+# read in each .csv file in file_list and create a data frame with the same name as the .csv file
+for (i in 1:length(file_list)){
+  assign(list.filenames[i], 
+         read.csv(paste(folder, file_list[i], sep=''))
+  )}
