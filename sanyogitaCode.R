3d20acdb0c8d0bfb277cbed9f759765f54702ff4
sanyogitapatil <39790766+sanyogitapatil@users.noreply.github.com>
2018-06-07 17:16:58 +0530

    Update sanyogitaCode.R

--- sanyogitaCode.R
+++ sanyogitaCode.R
@@ -14,3 +14,17 @@ for (i in 1:length(file_list)){
 
 Ambul_surg_measure <- left_join(left_join(`Ambulatory-Surgical-Measures-Facility`,`Ambulatory-Surgical-Measures-National`,c("Year")),`Ambulatory-Surgical-Measures-State`,c("State","Year"))
 Complications<-left_join(left_join(`Complications-Hospital`,`Complications-National`,c("Measure.Name","Measure.ID")),`Complications-State`,c("State","Measure.Name","Measure.ID"))
+###Looking at files of hvbp They have identical rows Even first 6 cols are same so good to combine them via merge
+##Two df hvbp_Efficiency_08_26_2016 and hvbp_imm2_08_26_2016 have col name as County_name and rest has county.Name
+##so changed the col name 
+colnames(hvbp_Efficiency_08_26_2016)[7] <- "County.Name"
+colnames(hvbp_imm2_08_26_2016)[7] <- "County.Name"
+
+hvbp<-merge(hvbp_ami_08_26_2016,hvbp_Efficiency_08_26_2016)
+hvbp<-merge(hvbp,hvbp_hai_08_26_2016)
+hvbp<-merge(hvbp,hvbp_hcahps_08_26_2016)
+hvbp<-merge(hvbp,hvbp_imm2_08_26_2016)
+hvbp<-merge(hvbp,hvbp_outcome_08_26_2016)
+hvbp<-merge(hvbp,hvbp_pn_08_26_2016)
+hvbp<-merge(hvbp,hvbp_scip_08_26_2016)
+hvbp<-merge(hvbp,hvbp_tps_08_26_2016)
